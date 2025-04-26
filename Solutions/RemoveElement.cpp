80cce8e58c3aadeb53a983c8cce26ad6a3d56f34
Ayman Hazem <144857540+AymanHazem@users.noreply.github.com>
2025-04-26 15:41:36 +0300

    Add files via upload

---
+++ Solutions/RemoveElement.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int removeElement(vector<int>& nums, int val)
+{
+    for (int i=0;i<nums.size();++i)
+    {
+        if (nums[i]==val)
+        {
+            nums.erase(nums.begin()+i);
+            i--;
+        }
+    }
+    return nums.size();
+}
